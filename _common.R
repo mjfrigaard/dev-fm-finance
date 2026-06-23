a55600d593cc910b3d63f8ccd38d59a3f746f5c6
Martin Frigaard <mjfrigaard@pm.me>
2026-06-23 14:42:36 -0700

    updates to tracking spending

--- _common.R
+++ _common.R
@@ -40,6 +40,15 @@ options(
   return(!(x %in% y))
 }
 
+# Formatting utilities ----
+fmt_dollar <- function(x, digits = 2) {
+  paste0("$", formatC(x, format = "f", digits = digits, big.mark = ","))
+}
+
+fmt_pct <- function(x, digits = 1) {
+  paste0(round(x * 100, digits), "%")
+}
+
 co_box <- function(
   color = "b",
   header = "header",
